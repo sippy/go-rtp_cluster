4682d771d1859dda6053eac94b7ff8d14b148ba9
Maksym Sobolyev <sobomax@sippysoft.com>
2023-10-04 23:41:15 +0000

    Run in docker.
    Run go get outside of docker.

--- scripts/travis/test_run.sh
+++ scripts/travis/test_run.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 set -e
+set -x
 
 BASEDIR="${BASEDIR:-$(dirname -- "${0}")/../..}"
 BASEDIR="$(readlink -f -- ${BASEDIR})"
 
+SHELL="${SHELL:-"/bin/sh"}"
+
 . ${BASEDIR}/scripts/travis/functions.sub
 
 cd ${TESTSDIR}
+ls -l
 #init_mr_time
 RTPPROXY_CMD="${RTPPROXY_BIN}" exec ${SHELL} basic_network
